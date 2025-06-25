3fefe4175c9505b0fdd17f16355d2023e44dff62
Rod Chang <rodchang1212@gmail.com>
2025-06-25 18:55:26 +0800

    fix : 修改 repo 名稱不相符

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Artale-Scroll/', // 設定 GitHub Pages 的基礎路徑
+  base: '/Artale_Scroll/',
   resolve: {
     alias: {
       '@': '/src',
     },
   },
   build: {
-    // 增加 chunk 大小警告閾值 (單位為 kB)
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
-        // 自定義分塊策略
         manualChunks: {
-          // 將 Vue 相關庫分到一個文件中
           'vue-vendor': ['vue', 'vue-router'],
-          // 將 Ant Design 相關庫分到一個文件中
           'ant-design-vendor': ['ant-design-vue', '@ant-design/icons-vue'],
         },
       },
